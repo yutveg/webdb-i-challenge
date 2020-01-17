94b7b7ff9fcf71d82010c09e8f818fabc59576b5
yutveg <kodi.0n31l@gmail.com>
2020-01-17 15:26:04 -0800

    all sql crud functionality good

--- accounts/router.js
+++ accounts/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
+    res.json(await db("accounts").select());
   } catch (err) {
     next(err);
   }
@@ -11,13 +12,19 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   try {
+    res.json(
+      await db("accounts")
+        .where("id", req.params.id)
+        .select()
+    );
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/", validateAccount, async (req, res, next) => {
   try {
+    res.json(await db("accounts").insert(req.body));
   } catch (err) {
     next(err);
   }
@@ -25,6 +32,11 @@ router.post("/", async (req, res, next) => {
 
 router.put("/:id", async (req, res, next) => {
   try {
+    res.json(
+      await db("accounts")
+        .where("id", req.params.id)
+        .update(req.body)
+    );
   } catch (err) {
     next(err);
   }
@@ -32,7 +44,36 @@ router.put("/:id", async (req, res, next) => {
 
 router.delete("/:id", async (req, res, next) => {
   try {
+    res.json(
+      await db("accounts")
+        .where("id", req.params.id)
+        .del()
+    );
   } catch (err) {
     next(err);
   }
 });
+
+function validateAccount(req, res, next) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ error: "No data received from client post." });
+  } else if (!req.body.name || !req.body.budget) {
+    res
+      .status(400)
+      .json({ error: "One or more required fields not received." });
+  } else if (!Number.isInteger(req.body.budget)) {
+    res.status(400).json({ error: "Wrong data type sent in budget field." });
+  } else next();
+}
+
+// function validateAccountId(req, res, next) {
+//   if (
+//     db("accounts")
+//       .where("id", req.params.id)
+//       .select()
+//   ) {
+//     res.status(404).json({ error: "That account is not on file." });
+//   } else next();
+// }
+
+module.exports = router;
