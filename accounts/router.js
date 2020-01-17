9f4f1dfb614e26eb37cfe34a2bf9f5429079f3b0
yutveg <kodi.0n31l@gmail.com>
2020-01-17 14:54:45 -0800

    initial SQL queries done

---
+++ accounts/router.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const db = require("../data/dbConfig.js");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
